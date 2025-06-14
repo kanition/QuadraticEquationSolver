b3c0f039d52d3b593a070ff04951dca8d74a7436
kanition <68691995+kanition@users.noreply.github.com>
2025-06-15 00:05:08 +0800

    fix warning C4566

--- test/double_test.cpp
+++ test/double_test.cpp
@@ -25,34 +25,34 @@ void test_double()
     demo("bx + c = 0, underflow", 0., q, p);
 
     demo("ax^2 = 0", 5., 0., 0.);
-    demo("ax^2 + c = 0, Δ < 0", 5., 0., 7.);
-    demo("ax^2 + c = 0, Δ < 0, |a| ~= 0, |c| ~= 0", p, 0., p);
-    demo("ax^2 + c = 0, Δ < 0, |a| >> 0, |c| >> 0", q, 0., q);
-    demo("ax^2 + c = 0, Δ < 0, |a| >> 0, |c| ~= 0", q, 0., p);
-    demo("ax^2 + c = 0, Δ < 0, |a| ~= 0, |c| >> 0", p, 0., q);
+    demo("ax^2 + c = 0, delta < 0", 5., 0., 7.);
+    demo("ax^2 + c = 0, delta < 0, |a| ~= 0, |c| ~= 0", p, 0., p);
+    demo("ax^2 + c = 0, delta < 0, |a| >> 0, |c| >> 0", q, 0., q);
+    demo("ax^2 + c = 0, delta < 0, |a| >> 0, |c| ~= 0", q, 0., p);
+    demo("ax^2 + c = 0, delta < 0, |a| ~= 0, |c| >> 0", p, 0., q);
 
-    demo("ax^2 + c = 0, Δ > 0", 1., 0., -9.);
-    demo("ax^2 + c = 0, Δ > 0, avoid overflow", 1e307, 0., -9e307);
-    demo("ax^2 + c = 0, Δ > 0, avoid underflow", 1e-309, 0., -9e-309);
-    demo("ax^2 + c = 0, Δ > 0, avoid overflow", 1e-309, 0., -2.89e307);
-    demo("ax^2 + c = 0, Δ > 0, avoid underflow", 1e308, 0., -2.89e-308);
+    demo("ax^2 + c = 0, delta > 0", 1., 0., -9.);
+    demo("ax^2 + c = 0, delta > 0, avoid overflow", 1e307, 0., -9e307);
+    demo("ax^2 + c = 0, delta > 0, avoid underflow", 1e-309, 0., -9e-309);
+    demo("ax^2 + c = 0, delta > 0, avoid overflow", 1e-309, 0., -2.89e307);
+    demo("ax^2 + c = 0, delta > 0, avoid underflow", 1e308, 0., -2.89e-308);
 
     demo("ax^2 + bx = 0, exist x = 0", 4., 8., 0.);
     demo("ax^2 + bx = 0, exist x = 0", 4., -8., 0.);
     demo("ax^2 + bx = 0, exist x = 0, avoid overflow", 4e307, -8e307, 0.);
     demo("ax^2 + bx = 0, exist x = 0, avoid underflow", 4e-308, -8e-308, 0.);
 
-    demo("ax^2 + bx + c = 0, Δ < 0", 2., 8., 10.);
-    demo("ax^2 + bx + c = 0, Δ < 0, avoid underflow", 2e-308, 8e-308, 1e-307);
-    demo("ax^2 + bx + c = 0, Δ < 0, avoid underflow", 2e307, 8e307, 1e308);
+    demo("ax^2 + bx + c = 0, delta < 0", 2., 8., 10.);
+    demo("ax^2 + bx + c = 0, delta < 0, avoid underflow", 2e-308, 8e-308, 1e-307);
+    demo("ax^2 + bx + c = 0, delta < 0, avoid underflow", 2e307, 8e307, 1e308);
 
-    demo("ax^2 + bx + c = 0, Δ = 0", 2., -8., 8.);
-    demo("ax^2 + bx + c = 0, Δ = 0, avoid overflow", 2e307, -8e307, 8e307);
-    demo("ax^2 + bx + c = 0, Δ = 0, avoid underflow", 3e-308, -1.2e-307, 1.2e-307);
+    demo("ax^2 + bx + c = 0, delta = 0", 2., -8., 8.);
+    demo("ax^2 + bx + c = 0, delta = 0, avoid overflow", 2e307, -8e307, 8e307);
+    demo("ax^2 + bx + c = 0, delta = 0, avoid underflow", 3e-308, -1.2e-307, 1.2e-307);
 
-    demo("ax^2 + bx + c = 0, Δ > 0", 6., -33., 45.);
-    demo("ax^2 + bx + c = 0, Δ > 0, avoid overflow", 6e306, -3.3e307, 4.5e307);
-    demo("ax^2 + bx + c = 0, Δ > 0, avoid underflow", 6e-308, -3.3e-307, 4.5e-307);
+    demo("ax^2 + bx + c = 0, delta > 0", 6., -33., 45.);
+    demo("ax^2 + bx + c = 0, delta > 0, avoid overflow", 6e306, -3.3e307, 4.5e307);
+    demo("ax^2 + bx + c = 0, delta > 0, avoid underflow", 6e-308, -3.3e-307, 4.5e-307);
 
     demo("Golden Ratio is 0.618033988749894(8)", 1., 1., -1.);
     demo("Golden Ratio is 0.618033988749894(8), avoid overflow", 1.e307, 1.e307, -1.e307);
